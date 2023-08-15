30483dbf1c3b04c9029255b861f66580c71d698a
Miguel Figueroa <miguelfigueroa90@gmail.com>
2023-08-15 10:14:38 -0400

    updating SeedDatabase command

--- app/Console/Commands/SeedDatabase.php
+++ app/Console/Commands/SeedDatabase.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Models\IngredientReceipt;
 use App\Models\Receipt;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 
 class SeedDatabase extends Command
 {
@@ -30,134 +31,116 @@ class SeedDatabase extends Command
         $receipt = new Receipt();
         $receipt->name = 'Meat with Tomato';
         $receipt->save();
-
-        $ingredientReceipt = new IngredientReceipt();
-        $ingredientReceipt->ingredient_id = 1; // Tomato
-        $ingredientReceipt->receipt_id = $receipt->id;
-        $ingredientReceipt->quantity = 1;
-        $ingredientReceipt->save();
-
-        $ingredientReceipt = new IngredientReceipt();
-        $ingredientReceipt->ingredient_id = 9; // Meat
-        $ingredientReceipt->receipt_id = $receipt->id;
-        $ingredientReceipt->quantity = 2;
-        $ingredientReceipt->save();
-        
-        $ingredientReceipt = new IngredientReceipt();
-        $ingredientReceipt->ingredient_id = 7; // Onion
-        $ingredientReceipt->receipt_id = $receipt->id;
-        $ingredientReceipt->quantity = 3;
-        $ingredientReceipt->save();
-
-        $ingredientReceipt = new IngredientReceipt();
-        $ingredientReceipt->ingredient_id = 5; // Ketchup
-        $ingredientReceipt->receipt_id = $receipt->id;
-        $ingredientReceipt->quantity = 1;
-        $ingredientReceipt->save();
+        DB::table('ingredient_receipt')->insert([
+            'ingredient_id' => 1, // Tomato
+            'receipt_id' => $receipt->id,
+            'quantity' => 1,
+        ]);
+        DB::table('ingredient_receipt')->insert([
+            'ingredient_id' => 9, // Meat
+            'receipt_id' => $receipt->id,
+            'quantity' => 2,
+        ]);
+        DB::table('ingredient_receipt')->insert([
+            'ingredient_id' => 7, // Onion
+            'receipt_id' => $receipt->id,
+            'quantity' => 3,
+        ]);
+        DB::table('ingredient_receipt')->insert([
+            'ingredient_id' => 5, // Ketchup
+            'receipt_id' => $receipt->id,
+            'quantity' => 1,
+        ]);
 
         $receipt = new Receipt();
         $receipt->name = 'Beef stew with potatoes';
         $receipt->save();
-
-        $ingredientReceipt = new IngredientReceipt();
-        $ingredientReceipt->ingredient_id = 9; // Meat
-        $ingredientReceipt->receipt_id = $receipt->id;
-        $ingredientReceipt->quantity = 2;
-        $ingredientReceipt->save();
-
-        $ingredientReceipt = new IngredientReceipt();
-        $ingredientReceipt->ingredient_id = 3; // Potato
-        $ingredientReceipt->receipt_id = $receipt->id;
-        $ingredientReceipt->quantity = 3;
-        $ingredientReceipt->save();
-
-        $ingredientReceipt = new IngredientReceipt();
-        $ingredientReceipt->ingredient_id = 7; // Onion
-        $ingredientReceipt->receipt_id = $receipt->id;
-        $ingredientReceipt->quantity = 1;
-        $ingredientReceipt->save();
+        DB::table('ingredient_receipt')->insert([
+            'ingredient_id' => 9, // Meat
+            'receipt_id' => $receipt->id,
+            'quantity' => 2,
+        ]);
+        DB::table('ingredient_receipt')->insert([
+            'ingredient_id' => 3, // Potato
+            'receipt_id' => $receipt->id,
+            'quantity' => 3,
+        ]);
+        DB::table('ingredient_receipt')->insert([
+            'ingredient_id' => 7, // Onion
+            'receipt_id' => $receipt->id,
+            'quantity' => 1,
+        ]);
 
         $receipt = new Receipt();
         $receipt->name = 'tomato and Lettuce salad';
         $receipt->save();
-
-        $ingredientReceipt = new IngredientReceipt();
-        $ingredientReceipt->ingredient_id = 1; // Tomato
-        $ingredientReceipt->receipt_id = $receipt->id;
-        $ingredientReceipt->quantity = 2;
-        $ingredientReceipt->save();
-
-        $ingredientReceipt = new IngredientReceipt();
-        $ingredientReceipt->ingredient_id = 6; // Lettuce
-        $ingredientReceipt->receipt_id = $receipt->id;
-        $ingredientReceipt->quantity = 3;
-        $ingredientReceipt->save();
-
-        $ingredientReceipt = new IngredientReceipt();
-        $ingredientReceipt->ingredient_id = 2; // Lemon
-        $ingredientReceipt->receipt_id = $receipt->id;
-        $ingredientReceipt->quantity = 1;
-        $ingredientReceipt->save();
+        DB::table('ingredient_receipt')->insert([
+            'ingredient_id' => 1, // Tomato
+            'receipt_id' => $receipt->id,
+            'quantity' => 2,
+        ]);
+        DB::table('ingredient_receipt')->insert([
+            'ingredient_id' => 6, // Lettuce
+            'receipt_id' => $receipt->id,
+            'quantity' => 3,
+        ]);
+        DB::table('ingredient_receipt')->insert([
+            'ingredient_id' => 2, // Lemon
+            'receipt_id' => $receipt->id,
+            'quantity' => 1,
+        ]);
 
         $receipt = new Receipt();
         $receipt->name = 'Rice with chicken and mild cheese sauce';
         $receipt->save();
-
-        $ingredientReceipt = new IngredientReceipt();
-        $ingredientReceipt->ingredient_id = 4; // Rice
-        $ingredientReceipt->receipt_id = $receipt->id;
-        $ingredientReceipt->quantity = 2;
-        $ingredientReceipt->save();
-
-        $ingredientReceipt = new IngredientReceipt();
-        $ingredientReceipt->ingredient_id = 10; // Chicken
-        $ingredientReceipt->receipt_id = $receipt->id;
-        $ingredientReceipt->quantity = 3;
-        $ingredientReceipt->save();
-
-        $ingredientReceipt = new IngredientReceipt();
-        $ingredientReceipt->ingredient_id = 8; // Cheese
-        $ingredientReceipt->receipt_id = $receipt->id;
-        $ingredientReceipt->quantity = 1;
-        $ingredientReceipt->save();
+        DB::table('ingredient_receipt')->insert([
+            'ingredient_id' => 4, // Rice
+            'receipt_id' => $receipt->id,
+            'quantity' => 2,
+        ]);
+        DB::table('ingredient_receipt')->insert([
+            'ingredient_id' => 10, // Chicken
+            'receipt_id' => $receipt->id,
+            'quantity' => 3,
+        ]);
+        DB::table('ingredient_receipt')->insert([
+            'ingredient_id' => 8, // Cheese
+            'receipt_id' => $receipt->id,
+            'quantity' => 1,
+        ]);
 
         $receipt = new Receipt();
         $receipt->name = 'Mexican style rice with chicken';
         $receipt->save();
-
-        $ingredientReceipt = new IngredientReceipt();
-        $ingredientReceipt->ingredient_id = 10; // Chicken
-        $ingredientReceipt->receipt_id = $receipt->id;
-        $ingredientReceipt->quantity = 2;
-        $ingredientReceipt->save();
-
-        $ingredientReceipt = new IngredientReceipt();
-        $ingredientReceipt->ingredient_id = 8; // Cheese
-        $ingredientReceipt->receipt_id = $receipt->id;
-        $ingredientReceipt->quantity = 3;
-        $ingredientReceipt->save();
+        DB::table('ingredient_receipt')->insert([
+            'ingredient_id' => 10, // Chicken
+            'receipt_id' => $receipt->id,
+            'quantity' => 2,
+        ]);
+        DB::table('ingredient_receipt')->insert([
+            'ingredient_id' => 8, // Cheese
+            'receipt_id' => $receipt->id,
+            'quantity' => 3,
+        ]);
 
         $receipt = new Receipt();
         $receipt->name = 'Green rice roll with chicken and cheese';
         $receipt->save();
-
-        $ingredientReceipt = new IngredientReceipt();
-        $ingredientReceipt->ingredient_id = 4; // Rice
-        $ingredientReceipt->receipt_id = $receipt->id;
-        $ingredientReceipt->quantity = 1;
-        $ingredientReceipt->save();
-
-        $ingredientReceipt = new IngredientReceipt();
-        $ingredientReceipt->ingredient_id = 10; // Chicken
-        $ingredientReceipt->receipt_id = $receipt->id;
-        $ingredientReceipt->quantity = 2;
-        $ingredientReceipt->save();
-
-        $ingredientReceipt = new IngredientReceipt();
-        $ingredientReceipt->ingredient_id = 8; // Cheese
-        $ingredientReceipt->receipt_id = $receipt->id;
-        $ingredientReceipt->quantity = 3;
-        $ingredientReceipt->save();
+        DB::table('ingredient_receipt')->insert([
+            'ingredient_id' => 4, // Rice
+            'receipt_id' => $receipt->id,
+            'quantity' => 1,
+        ]);
+        DB::table('ingredient_receipt')->insert([
+            'ingredient_id' => 10, // Chicken
+            'receipt_id' => $receipt->id,
+            'quantity' => 2,
+        ]);
+        DB::table('ingredient_receipt')->insert([
+            'ingredient_id' => 8, // Cheese
+            'receipt_id' => $receipt->id,
+            'quantity' => 3,
+        ]);
 
         $this->info('The command was successful!');
     }
