b2b36a748f5e2ba5f9e00ecb00e6510148d89569
Miguel Figueroa <miguelfigueroa90@gmail.com>
2023-08-17 00:56:45 -0400

    Receipts menu for mobile

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -114,6 +114,12 @@
             </x-responsive-nav-link>
         </div>
 
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('receipts.index')" :active="request()->routeIs('receipts.index')">
+                {{ __('Receipts') }}
+            </x-responsive-nav-link>
+        </div>
+
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="px-4">
