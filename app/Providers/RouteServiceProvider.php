6b90bfa155b44e3c1f23f0404d3f4f3e3f095fcb
Miguel Figueroa <miguelfigueroa90@gmail.com>
2023-08-17 04:44:30 -0400

    Disabling dashboard

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/created_orders';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,12 +12,6 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                </div>
-
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('created_orders.index')" :active="request()->routeIs('created_orders.index')">
                         {{ __('Created orders') }}
                     </x-nav-link>
@@ -90,12 +84,6 @@
 
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-        </div>
-
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('created_orders.index')" :active="request()->routeIs('created_orders.index')">
                 {{ __('Created orders') }}
